b39acb4c90e104ec10562ef92ca09013b81a98ff
= <=>
2026-03-16 13:22:42 +0100

    Ajout cors OK

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 const PORT = process.env.PORT;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
